151e7a4efca073ef43ab8a268395c4b235954da2
Brutspark <38699287+brutspark@users.noreply.github.com>
2021-05-12 22:22:57 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,6 +9,41 @@ int main(void){
 	clock1.clock(1);
 
 
+	ifstream CLK_IN;
+	
+	ifstream button_in;
+
+	ofstream led_out;
+
+	CLK_IN.open("clk_out.bin");
+
+	button_in.open("button.bin");
+	
+	led_out.open("led_out.bin");
+
+	char in_a = 0;
+	char in_b = 0;
+
+	while(button_in.eof() ){
+		
+		button_in.get(in_a);
+		CLK_IN.get(in_b);
+
+		led_out << ( in_a & in_b);
+
+
+		cout << in_a << endl;
+		cout << in_b << endl;
+
+	}
+	
+	CLK_IN.close();
+	button_in.close();
+	led_out.close();
+	
+		
+	
+
 
 return 0;
 }
